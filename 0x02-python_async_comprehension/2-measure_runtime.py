c61a8cd1caf1460a486d920bdd6b4aada11410ec
knia10 <kelly.stephania@hotmail.com>
2022-09-21 20:52:47 -0500

    correction style

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -17,8 +17,7 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
-    ''' 
-    
+    '''
     a measure_runtime coroutine that will execute async_comprehension
     four times in parallel using asyncio.gather
     '''
